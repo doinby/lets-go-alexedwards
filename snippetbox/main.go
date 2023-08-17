9e83c472a8bedc6af1fa5d6cb6dc7198ad11aae9
Chinh Do <chinh@doinby.co>
2023-08-17 10:25:51 +1000

    Chapter 2.2. Web application basics

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox!"))
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting server on :3000")
+	err := http.ListenAndServe(":3000", mux)
+	log.Fatal(err)
 }
