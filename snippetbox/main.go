33093cce3de0560ddc1abbeef995c020cc8bbeb8
Chinh Do <chinh@doinby.co>
2023-08-17 12:51:50 +1000

    2.3. Routing Request

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -5,13 +5,34 @@ import (
 	"net/http"
 )
 
+// Index and 404 page
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/".
+	// If not, run http.NotFound() function as response
+	// Importantly, we then return from the handler. If we don't return the handler
+	// would keep executing and also write the "Hello from SnippetBox" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :3000")
 	err := http.ListenAndServe(":3000", mux)
