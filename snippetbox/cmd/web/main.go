45a0b1158b38f890ed67fc0068471a2fa9f9142b
Chinh Do <chinh@doinby.co>
2023-08-18 22:02:39 +1000

    3.2. Leveled logging: The http.Server error log - http.Server()

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -40,7 +40,17 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
+	// Initialize a new http.Server struct,
+	// set the Addr and Handler fields so
+	// server uses same network address and routes as before
+	// and set the ErrorLog field to use custom errorLog logger
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	infoLog.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
