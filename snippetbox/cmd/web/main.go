425838550b49663bbb42c89c90494c59afcf0f87
Chinh Do <chinh@doinby.co>
2023-08-20 11:42:15 +1000

    3.5. Isolating the Application Routes - Abstracting routing in `main.go` by moving mux instantiation to `routes.go`

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -39,21 +39,6 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	// Routes - uses application struct's method as handler func
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// Initialize a new http.Server struct,
 	// set the Addr and Handler fields so
 	// server uses same network address and routes as before
@@ -61,7 +46,7 @@ func main() {
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(), // from ./routes.go
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
